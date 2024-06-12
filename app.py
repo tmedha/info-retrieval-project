65626f5e31a7a535ac0906770e8e06f721ec5c20
Medha Tripathi <tmedha01@gmail.com>
2024-06-12 15:19:09 -0500

    Add Part 1

---
+++ app.py
@@ -0,0 +1,46 @@
+# Medha Tripathi (Student ID: 20655230)
+# Samyuktha Reddy Gurram (Student ID: 20719222)
+# Suyesh Bhatta (Student ID: 20672078)
+
+
+
+from zipfile import ZipFile
+from bs4 import BeautifulSoup
+import os
+import re
+
+
+with ZipFile('Jan.zip', 'r') as zip:
+    zip.extractall('.')
+
+file_directory = 'Jan' 
+
+file_list = os.listdir(file_directory)
+index_map = {}
+
+for file_name in file_list:
+    file_path = os.path.join(file_directory, file_name)
+    
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+            soup = BeautifulSoup(content, 'html.parser')
+            text = soup.get_text().lower()
+            extracted_strings = re.findall(r'[a-z]+', text)
+            extracted_strings = set(extracted_strings)
+            index_map[file_name] = extracted_strings
+            
+while True:
+    search_key = input('Enter a search key=> ').strip()
+    matches = []
+    if search_key == '':
+        print('Bye.')
+        break
+    for file_name in index_map:
+        if search_key in index_map[file_name]:
+            matches.append(file_name)
+    if len(matches) > 0:
+        for file_name in matches:
+            print('Found a match:', file_name)
+    else:
+        print('No match.')
